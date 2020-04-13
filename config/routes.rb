d6dbb259340868e1f657a528ad6f709d9da01ca7
enakata <edson.nakata@webtechbr.com>
2020-04-13 18:32:30 -0400

    Criado endpoints para criar albums e musicas recentes

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,11 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
+      resources :albums, only: :show do
+        resources :recently_heards, only: %i[create]
+      end
+      resources :categories, only: %i[index show]
       resources :dashboard, only: :index
-      resources :categories, only: [:index, :show]
       resources :search, only: :index
     end
   end
