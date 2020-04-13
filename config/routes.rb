88f7ebc434d7fd933d281a647537be2d6651419b
enakata <edson.nakata@webtechbr.com>
2020-04-13 19:16:40 -0400

    Finalizaçao do backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
-  root "home#index"
+  root 'home#index'
+
+  concern :favoritable do |options|
+    shallow do
+      post '/favorite', {to: 'favorites#create', on: :member}.merge(options)
+      delete '/favorite', {to: 'favorites#destroy', on: :member}.merge(options)
+    end
+  end
 
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
@@ -10,7 +17,11 @@ Rails.application.routes.draw do
       end
       resources :categories, only: %i[index show]
       resources :dashboard, only: :index
+      resources :favorites, only: :index
       resources :search, only: :index
+      resources :songs, only: [] do
+        concerns :favoritable, favoritable_type: 'Song'
+      end
     end
   end
 
