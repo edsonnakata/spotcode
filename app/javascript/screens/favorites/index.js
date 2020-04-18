045db1fb12e5daf6dd0c68f8cf276b6bfcf7e6df
enakata <edson.nakata@webtechbr.com>
2020-04-18 09:48:16 -0400

    Criado a navbar_footer

--- app/javascript/screens/favorites/index.js
+++ app/javascript/screens/favorites/index.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from 'react';
+import NavbarFooter from '../../components/shared/navbar_footer';
 
 const FavoritesScreen = () => {
   return(
     <Fragment>
       <div>Favorites Screen</div>
+      <NavbarFooter/>
     </Fragment>
   )
 }
--- app/javascript/screens/search/index.js
+++ app/javascript/screens/search/index.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from 'react';
+import NavbarFooter from '../../components/shared/navbar_footer';
 
 const SearchScreen = () => {
   return(
     <Fragment>
       <div>Search Screen</div>
+      <NavbarFooter/>
     </Fragment>
   )
 }
