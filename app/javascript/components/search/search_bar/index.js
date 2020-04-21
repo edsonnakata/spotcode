b5ccadb161387093687cd80c7243971ac8b83cfd
enakata <edson.nakata@webtechbr.com>
2020-04-21 12:17:19 -0400

    Criando os components Categories e SearchBar

--- app/javascript/components/search/search_bar/index.js
+++ app/javascript/components/search/search_bar/index.js
@@ -1,9 +1,27 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
+import { Form } from 'react-bulma-components';
 
-const SearchBar = () => {
+const SearchBar = (props) => {
+  const [query, setQuery] = useState("");
+
+  const Search = (e) => {
+    if (e.key === 'Enter') {
+      props.fetchSearch(query);
+      setQuery("")
+    }
+  }
+
   return(
     <Fragment>
-
+      <Form.Field onKeyDown={Search}>
+        <Form.Control iconRight>
+          <Form.Input
+            placeholder='Álbuns, artistas ou músicas'
+            value={query}
+            onChange={e => setQuery(e.target.value)}
+          />
+        </Form.Control>
+      </Form.Field>
     </Fragment>
   );
 }
